33b040b57b92a43673b0261d166efb115e2a92c4
joshua simpas <joshsimpas6@gmail.com>
2025-03-20 12:01:25 +0800

    battleship v2

---
+++ Getting Functional/function.js
@@ -0,0 +1,27 @@
+var dogName = "rover";
+var dogWeight = 23;
+if (dogWeight > 20) {
+    console.log(dogName + "says WOOF WOOF");
+} else {
+    console.log(dogName + "says WOOF WOOF");
+}
+
+dogName = "spot";
+dogWieght = 12;
+if (dogWeight > 20) {
+    console.log(dogName + "says WOOF WOOF");
+} else {
+    console.log(dogName + "says WOOF WOOF");
+}
+
+function bark(name, weight){
+    if (weight > 20) {
+        console.log(name + "says WOOF WOOF");
+    } else {
+        console.log(name + "says WOOF WOOF");
+    }
+}
+
+bark("rover", 23);
+bark("spor", 12);
+bark("spike", 53);
---
+++ battleship_v1.js
@@ -0,0 +1,35 @@
+// Declared variables for the game
+
+var location1 = 3;
+var location2 = 4;
+var location3 = 5;
+
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+
+// Game logic
+
+while (isSunk == false) {
+    guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
+    if (guess <0 || guess > 6){
+        alert("Please enter a valid cell number!");
+    } else {
+        guesses = guesses + 1;
+        if (guess == location1 || guess == location2 || guess == location3){
+            alert ("HIT!");
+            hits = hits + 1;
+            if (hits == 3){
+                isSunk = true;
+                alert("You sank my battleship!");
+
+            }
+        } else {
+            alert("MISS!");
+        }
+    }
+}
+
+var stats = "You sank my battleship at " + guesses + " guesses to sink the battleship, "+"which means your shooting acurracy was " + (3/guesses);
+alert(stats);
